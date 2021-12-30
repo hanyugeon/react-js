ef8e9f21b2b9f3c9fc69beb6ab4816113f9c147d
hanyugeon <hanyugeon@naver.com>
2021-12-31 03:12:38 +0900

    21.12.31 JSX, 인라인 스타일링, className 활용

--- hello_ReactJS/hello-react/src/App.js
+++ hello_ReactJS/hello-react/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const name = '리액트';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '24px',
+    fontWeight: 'bold',
+    padding: 16
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header>
+      <div className="react">
+        {name}
+      </div>
+      <div style={style}>
+        <h2>잘 작동하니?</h2>
+        {name === '리액트' ? (
+          <h3>리액트가 맞습니다.</h3>
+        ) : (
+          null
+        )}
+      </div>
+    </header>
+    
+  )
 }
 
 export default App;
