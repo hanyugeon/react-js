df65be44d9eb723360d0686e68bcdd61a5b51caa
hanyugeon <hanyugeon@naver.com>
2022-01-01 20:35:59 +0900

    21.01.01 클래스형 컴포넌트 생성 및 내보내기, 불러오기

--- hello_ReactJS/hello-react/src/App.js
+++ hello_ReactJS/hello-react/src/App.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import './App.css';
+import MyComponent from './MyComponent';
 
-function App() {
-  const name = '리액트';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '24px',
-    fontWeight: 'bold',
-    padding: 16,
-  };
-
-  return (
-    <header>
-      <div className="react">{name}</div>
-      <div style={style}>
-        <h2>잘 작동하니?</h2>
-        {name === '리액트' ? <h3>리액트가 맞습니다.</h3> : null}
-      </div>
-      <input /> {/* self-closing 태그 & react 주석 쓰는 방법*/}
-    </header>
-  );
-}
+const App = () => {
+  return <MyComponent />;
+};
 
 export default App;
