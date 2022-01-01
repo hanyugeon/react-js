d72851db4e8a49e84479d25c070942af6ae07cf5
hanyugeon <hanyugeon@naver.com>
2022-01-01 18:32:16 +0900

    22.01.01 self-closing, 주석, 환경설정(ESLint, Prettier)

--- hello_ReactJS/hello-react/src/App.js
+++ hello_ReactJS/hello-react/src/App.js
@@ -8,25 +8,19 @@ function App() {
     color: 'aqua',
     fontSize: '24px',
     fontWeight: 'bold',
-    padding: 16
+    padding: 16,
   };
 
   return (
     <header>
-      <div className="react">
-        {name}
-      </div>
+      <div className="react">{name}</div>
       <div style={style}>
         <h2>잘 작동하니?</h2>
-        {name === '리액트' ? (
-          <h3>리액트가 맞습니다.</h3>
-        ) : (
-          null
-        )}
+        {name === '리액트' ? <h3>리액트가 맞습니다.</h3> : null}
       </div>
+      <input /> {/* self-closing 태그 & react 주석 쓰는 방법*/}
     </header>
-    
-  )
+  );
 }
 
 export default App;
