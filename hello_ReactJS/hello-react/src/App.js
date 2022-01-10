4ff256fd600768447bad94f7703dc010a065e553
hanyugeon <hanyugeon@naver.com>
2022-01-10 22:43:28 +0900

    21.01.10 propTypes 와 isRequired

--- hello_ReactJS/hello-react/src/App.js
+++ hello_ReactJS/hello-react/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import MyClassComponent from './MyClassComponent';
 import MyComponent from './MyComponent';
 
 const App = () => {
   return (
-    <MyComponent name={'React'} review={'Review'} type={'PropTypes:'}>
+    <MyComponent
+      name={'React'}
+      review={'Review'}
+      type={'PropTypes:'}
+      favoriteNumber={1}
+    >
       하이, 칠드런
     </MyComponent>
   );
--- hello_ReactJS/hello-react/src/MyComponent.js
+++ hello_ReactJS/hello-react/src/MyComponent.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, children, review, type }) => {
+const MyComponent = ({ name, favoriteNumber, children, review, type }) => {
   return (
     <div>
       안녕하세요, 제 이름은 {name}입니다. <br />
       children 값은 {children}
       입니다. <br />
       {review} 하는 중이에요. <br />
-      {type} PropTypes란? 콘솔창과 line23을 확인해봐.
+      {type} PropTypes란? 콘솔창과 line23을 확인해봐. <br />
+      제가 제일 좋아하는 숫자는 {favoriteNumber}입니다. <br />
+      <hr />
     </div>
   );
 };
@@ -20,6 +22,7 @@ MyComponent.defaultProps = {
 MyComponent.propTypes = {
   name: PropTypes.string,
   type: PropTypes.number,
+  favoriteNumber: PropTypes.number.isRequired,
 };
 
 export default MyComponent;
