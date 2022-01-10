a6ce397dff0c7f64d81183e44865d02259d29ecb
hanyugeon <hanyugeon@naver.com>
2022-01-10 22:09:08 +0900

    22.01.10 props 이전 내용 복습

--- hello_ReactJS/hello-react/src/App.js
+++ hello_ReactJS/hello-react/src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent name={'React'}>리액트</MyComponent>;
+  return (
+    <MyComponent name={'React'} review={'Review'} type={'PropTypes:'}>
+      하이, 칠드런
+    </MyComponent>
+  );
 };
 
 export default App;
--- hello_ReactJS/hello-react/src/MyComponent.js
+++ hello_ReactJS/hello-react/src/MyComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, children, review, type }) => {
   return (
     <div>
       안녕하세요, 제 이름은 {name}입니다. <br />
       children 값은 {children}
-      입니다.
+      입니다. <br />
+      {review} 하는 중이에요. <br />
+      {type} PropTypes란? 콘솔창과 line23을 확인해봐.
     </div>
   );
 };
@@ -17,6 +19,7 @@ MyComponent.defaultProps = {
 
 MyComponent.propTypes = {
   name: PropTypes.string,
+  type: PropTypes.number,
 };
 
 export default MyComponent;
