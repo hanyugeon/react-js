af4e1afb88884e0330ed54846039074d3d5b0136
hanyugeon <hanyugeon@naver.com>
2022-01-11 23:03:15 +0900

    21.01.11 클래스 컴포넌트에서의 state(setState)

--- hello_ReactJS/hello-react/src/App.js
+++ hello_ReactJS/hello-react/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import MyClassComponent from './MyClassComponent';
 import MyComponent from './MyComponent';
+import Counter from './Counter';
 
 const App = () => {
-  return (
-    <div>
-      <MyComponent
-        name={'React'}
-        review={'Review'}
-        type={'PropTypes:'}
-        favoriteNumber={1}
-      >
-        하이, 칠드런
-      </MyComponent>
-      <MyClassComponent what={'클래스'} num={2}></MyClassComponent>
-    </div>
-  );
+  return <Counter />;
+  // (
+  //   <div>
+  //     <MyComponent
+  //       name={'React'}
+  //       review={'Review'}
+  //       type={'PropTypes:'}
+  //       favoriteNumber={1}
+  //     >
+  //       하이, 칠드런
+  //     </MyComponent>
+  //     <MyClassComponent what={'클래스'} num={2}></MyClassComponent>
+  //   </div>
+  // );
 };
 
 export default App;
