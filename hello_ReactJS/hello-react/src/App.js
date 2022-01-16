728928b5547cc45516a8714396af461dc7645c25
hanyugeon <hanyugeon@naver.com>
2022-01-16 23:15:48 +0900

    22.01.16 콜백 함수를 이용한 ref 사용방법과 실습

--- hello_ReactJS/hello-react/src/App.js
+++ hello_ReactJS/hello-react/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import EventPractice from './EventPractice';
+import React, { Component } from 'react';
+import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
-const App = () => {
-  return <EventPractice />;
-};
+class App extends Component {
+  render() {
+    return (
+      // <ValidationSample />;
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
