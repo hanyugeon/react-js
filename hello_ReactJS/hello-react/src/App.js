2d2945afde09c82e156661575e13762bd063eda9
hanyugeon <hanyugeon@naver.com>
2022-01-20 02:46:57 +0900

    22.01.19 JS 함수들을 활용하여 컴포넌트 배열 반환 및 반복

--- hello_ReactJS/hello-react/src/App.js
+++ hello_ReactJS/hello-react/src/App.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
-import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
 class App extends Component {
   render() {
-    return (
-      // <ValidationSample />;
-      <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
