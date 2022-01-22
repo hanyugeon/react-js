98d343f71665949b688f61679231d182482728f1
hanyugeon <hanyugeon@naver.com>
2022-01-22 21:57:37 +0900

    22.01.22 컴포넌트 라이프 사이클 복습 및 에러 처리 방법

--- hello_ReactJS/hello-react/src/App.js
+++ hello_ReactJS/hello-react/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
 
+// 랜덤 색상을 생성합니다.
 function getRandomColor() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
@@ -20,7 +22,9 @@ class App extends Component {
     return (
       <div>
         <button onClick={this.handleClick}>랜덤 색상</button>
-        <LifeCycleSample color={this.state.color} />
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </div>
     );
   }
