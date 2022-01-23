d32e976d94e7ee95332227f4a53769b55a5b55b7
hanyugeon <hanyugeon@naver.com>
2022-01-23 20:32:56 +0900

    22.01.23 Hooks useState

--- hello_ReactJS/hooks-tutorial/src/App.js
+++ hello_ReactJS/hooks-tutorial/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Counter from "./Counter";
+import Info from "./Info";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <Info />;
+};
+
+// userState 함수는 하나의 상태 값만 관리 가능.
+// 따라서 관리해야할 상태가 여러 개라면 여러번 사용.
 
 export default App;
