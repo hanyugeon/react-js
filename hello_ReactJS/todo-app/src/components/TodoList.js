2f2aae8ad73c67b0e48cda9353aa92f133d9191e
hanyugeon <hanyugeon@naver.com>
2022-02-24 18:57:47 +0900

    feat: 항목 삭제 기능(TodoListItem에서 삭제 함수 호출하기)

--- hello_ReactJS/todo-app/src/components/TodoList.js
+++ hello_ReactJS/todo-app/src/components/TodoList.js
@@ -2,17 +2,15 @@ import React from "react";
 import TodoListItem from "./TodoListItem";
 import "./TodoList.scss";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove }) => {
   // props로 받아온 todos
+  // map을 사용, TodoListItem으로 이루어진 배열로 변환 및 렌더링
+  // 여러 종류의 값을 전달해야하는 경우, 객체로 통쨰로 전달하기. -> 이후 성능 최적화 편리
   return (
     <div className="TodoList">
-      {todos.map(
-        (
-          todo // map을 사용, TodoListItem으로 이루어진 배열로 변환 및 렌더링
-        ) => (
-          <TodoListItem todo={todo} key={todo.id} />
-        ) // 여러 종류의 값을 전달해야하는 경우, 객체로 통쨰로 전달하기. -> 이후 성능 최적화 편리
-      )}
+      {todos.map((todo) => (
+        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
+      ))}
     </div>
   );
 };
