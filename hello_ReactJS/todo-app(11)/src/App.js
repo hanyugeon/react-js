1549795ac69682e8e372dee8a9086e24f6126e86
hanyugeon <hanyugeon@naver.com>
2022-03-10 21:37:51 +0900

    22.03.10 11장 컴포넌트 성능 최적화 공부 시작

---
+++ hello_ReactJS/todo-app(11)/src/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useRef, useCallback } from "react";
+import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
+
+const App = () => {
+  // 초기 상태 설정
+  const [todos, setTodos] = useState([
+    // todos 라는 상태 정의, setTodos 세터함수.
+    {
+      id: 1,
+      text: "리액트의 기초 알아보기",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "컴포넌트 스타일링 해보기",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "일정 관리 앱 만들어 보기",
+      checked: false,
+    },
+  ]);
+
+  // 고윳값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checkd: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; // nextId 1씩 더하기
+    },
+    [todos]
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos]
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo
+        )
+      );
+    },
+    [todos]
+  );
+
+  // todos를 TodoList.js의 props로 넘겨주기
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+};
+
+export default App;
---
+++ hello_ReactJS/todo-app(11)/src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import TodoListItem from "./TodoListItem";
+import "./TodoList.scss";
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  // props로 받아온 todos
+  // map을 사용, TodoListItem으로 이루어진 배열로 변환 및 렌더링
+  // 여러 종류의 값을 전달해야하는 경우, 객체로 통쨰로 전달하기. -> 이후 성능 최적화 편리
+  return (
+    <div className="TodoList">
+      {todos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
