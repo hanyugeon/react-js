1b6f5ea9d9b7389b762de7fd31a8ac0a66cd2275
hanyugeon <hanyugeon@naver.com>
2022-04-07 16:28:45 +0900

    "22.04.07 핸들러 조작과 state 수정 방법"

--- hello_ReactJS/re-component-props-state/src/App.js
+++ hello_ReactJS/re-component-props-state/src/App.js
@@ -9,20 +9,43 @@ const App = () => {
   ]); // [a, b] a에는 state 데이터, b에는 데이터 변경 함수
   // state는 변수 대신 쓰는 데이터 저장 공간
   // 자주 바뀌는, 중요한 데이터를 변수 말고 state로 저장해서 쓰자!
+  const [like, setLike] = useState(0);
+  const editTitle = () => {
+    // referance data type의 특징으로 인한 deep copy활용
+    const tempArr = [...title];
+    // state를 직접 수정 할 수 없음.
+    tempArr[0] = "마른 여자 옷 추천";
+    tempArr[1] = "키작은 여자 옷 추천";
+    tempArr[2] = "귀여운 코디 모음집";
+    setTitle(tempArr);
+  };
+
   return (
     <div className="App">
       <div className="nav">
         <div>옷 추천 Blog</div>
       </div>
+      <button onClick={editTitle}>여성</button>
       <div className="list">
-        <h3> {title[0]} </h3>
-        <p>04월 06일 발행</p>
+        <h3>
+          {" "}
+          {title[0]}{" "}
+          <span
+            onClick={() => {
+              setLike(like + 1);
+            }}
+          >
+            👍
+          </span>{" "}
+          {like}{" "}
+        </h3>
+        <p>마지막 수정일 04월 07일</p>
         <hr />
         <h3> {title[1]} </h3>
-        <p>04월 06일 발행</p>
+        <p>마지막 수정일 04월 06일</p>
         <hr />
         <h3> {title[2]} </h3>
-        <p>04월 06일 발행</p>
+        <p>마지막 수정일 04월 06일</p>
         <hr />
       </div>
     </div>
