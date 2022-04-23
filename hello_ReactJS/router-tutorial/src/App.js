695456ee26b6b1b3fa7356303bf496598387e736
hanyugeon <hanyugeon@naver.com>
2022-04-23 17:01:13 +0900

    22.04.23 중첩 라우트에 대한 기초와 Outlet 컴포넌트 사용법

--- hello_ReactJS/router-tutorial/src/App.js
+++ hello_ReactJS/router-tutorial/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
+import Article from "./pages/Article";
+import Articles from "./pages/Articles";
 import Home from "./pages/Home";
 import Profile from "./Profile";
 
@@ -11,6 +13,10 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile/:username" element={<Profile />} />
+        <Route path="/articles" element={<Articles />}>
+          <Route path=":id" element={<Article />} />
+        </Route>
+        {/* <Route path="/articles/:id" element={<Article />} /> */}
       </Routes>
     </div>
   );
--- hello_ReactJS/router-tutorial/src/pages/Home.js
+++ hello_ReactJS/router-tutorial/src/pages/Home.js
@@ -8,16 +8,19 @@ const Home = () => {
       <p>가장 먼저 보여지는 페이지 입니다.</p>
       <ul>
         <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="profile/jaehyeon">김재현</Link>
+          <Link to="/profile/jaehyeon">김재현</Link>
         </li>
         <li>
-          <Link to="profile/yugeon">한유건</Link>
+          <Link to="/profile/yugeon">한유건</Link>
+        </li>
+        <li>
+          <Link to="/profile/void">존재하지 않는 프로필</Link>
+        </li>
+        <li>
+          <Link to="/articles">게시글 목록</Link>
         </li>
       </ul>
     </div>
