5ce71afe763379e374ac4b88071b0dfc8f5723d9
hanyugeon <hanyugeon@naver.com>
2022-04-26 23:04:12 +0900

    22.04.26 Layout 컴포넌트 생성, Outlet 컴포넌트를 활용한 헤더와 컨탠츠 부분 나타내기

--- hello_ReactJS/router-tutorial/src/App.js
+++ hello_ReactJS/router-tutorial/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
 import About from "./pages/About";
 import Article from "./pages/Article";
 import Articles from "./pages/Articles";
@@ -10,9 +11,11 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profile/:username" element={<Profile />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile/:username" element={<Profile />} />
+        </Route>
         <Route path="/articles" element={<Articles />}>
           <Route path=":id" element={<Article />} />
         </Route>
