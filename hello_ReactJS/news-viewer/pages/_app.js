c889b8ba33035443c4d50ed7ac43ca665f7b979d
hanyugeon <hanyugeon@naver.com>
2022-07-05 20:27:28 +0900

    22.07.05 뉴스뷰어 UI 구성

--- hello_ReactJS/news-viewer/pages/_app.js
+++ hello_ReactJS/news-viewer/pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import NewsList from '../components/NewsList';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
+
+function MyApp() {
+  return <NewsList />;
 }
 
-export default MyApp
+export default MyApp;
